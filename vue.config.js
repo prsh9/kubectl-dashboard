337a4ea61f80c9310daada458a0515bf9c8ea45c
Pratham Shah <pratham.shah9@gmail.com>
2019-10-09 01:04:35 +0530

    Packaging Config Changes

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,12 @@
 var webpack = require("webpack");
 
-function getEnvVariables() {
-    var custom_env = {};
-  for (var key in process.env) {
-    custom_env[key] = process.env[key];
-  }
-  console.log(custom_env);
-  return custom_env;
-}
-
 module.exports = {
     pluginOptions: {
         electronBuilder: {
             builderOptions: {
-                    "appId": "com.example.app"
-            }
+                "appId": "com.example.app"
+            },
+            externals: ["kubernetes-client"]
         }
     },
     configureWebpack: {
