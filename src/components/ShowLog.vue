f0a856613475e379f0a288697b8432c2d03268ab
Pratham Shah <pratham.shah9@gmail.com>
2020-03-22 00:20:13 +0530

    Support for changing container for logs

--- src/components/ShowLog.vue
+++ src/components/ShowLog.vue
@@ -2,9 +2,7 @@
   <div>
     <v-card>
       <v-card-title>
-        <h3>
-          Logs for <b class="text-capitalize">{{ podNamespace }} - {{ podName }}</b>
-        </h3>
+        <h3>Logs for <b class="text-capitalize">{{ podNamespace }} - {{ podName }}</b></h3>
         <v-layout row class="fab-horizontal">
           <v-btn color="cyan" depressed fab x-small class="btn-margin" @click="clearConsole">
             <v-icon>mdi-close</v-icon>
@@ -17,6 +15,15 @@
           </v-btn>
         </v-layout>
       </v-card-title>
+      <v-card-subtitle v-if="num_containers &gt; 1">
+        <v-select
+          v-model="currContainer"
+          :items="get_container_names"
+          menu-props="auto"
+          label="Select Container"
+          @change="containerChanged"></v-select>
+      </v-card-subtitle>
+
       <v-card class="overflow-y-auto" max-height="75vh" flat outlined>
         <v-card-text id="logWindow" ref="logwindow" max-height="75vh">
           <pre v-if="!connected">{{errMessage}}</pre>
@@ -44,26 +51,59 @@ export default {
       logdata: [],
       connected: false,
       errMessage: "",
-      wrap: true
+      wrap: true,
+      pod_spec: null,
+      currContainer: "",
+      num_containers: 0
     };
   },
+  computed: {
+    get_container_names: function() {
+      var container_names = [];
+      
+      if(this.pod_spec) {
+        this.pod_spec.spec.containers.forEach(element => {
+          container_names.push(element.name);
+        });
+      }
+
+      return container_names;
+    }
+  },
   mounted() {
-    this.startLogging();
+    this.init();
   },
   beforeDestroy() {
     this.stopLogging();
   },
   methods: {
-    startLogging: async function() {
+    init: async function() {
       const kubeclient = require("kubernetes-client");
       const client = new kubeclient.Client1_13();
       try {
         var pod_details = await client.api.v1.namespaces(this.podNamespace).pods(this.podName).get();
-        var container_name = pod_details.body.spec.containers[0].name;
+        this.pod_spec = pod_details.body;
+        this.num_containers = this.pod_spec.spec.containers.length;
+        this.currContainer = this.pod_spec.spec.containers[0].name;
 
+        this.startLogging();
+      } catch (e) {
+        this.connected = false;
+        this.errMessage = "[Error] " + e;
+      }
+    },
+    containerChanged: function(element) {
+      this.currContainer = element;
+      this.stopLogging();
+      this.startLogging();
+    },
+    startLogging: async function() {
+      const kubeclient = require("kubernetes-client");
+      const client = new kubeclient.Client1_13();
+      try {
         stream = await client.api.v1.namespaces(this.podNamespace).pods(this.podName).log.getByteStream({
           qs: {
-            container: container_name,
+            container: this.currContainer,
             tailLines: 10,
             follow: true
           }
