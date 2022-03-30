4b09640b27faf3ccf8b60befbc5cdb68096924bf
PrSh9 <>
2022-03-30 21:35:19 +0530

    Support for Namespace Filtering

--- src/components/DisplayNamespace.vue
+++ src/components/DisplayNamespace.vue
@@ -1,28 +1,32 @@
 <template>
   <div class="flexcontainer flex-grow-1">
-    <v-card tile flat>
-      <v-card-title class="pa-2">Namespaces</v-card-title>
-    </v-card>
+    <v-app-bar tile elevate-on-scroll color="white" class="top-sticky">
+      <v-toolbar-title class="pa-2">Namespaces</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-text-field clearable single-line hide-details outlined flat dense
+        placeholder="filter" class="pa-2 maxwidth" @input="filterNamespace">
+      </v-text-field>
+      <v-btn :disabled="!changed" @click="updateItem" ripple tile color="teal lighten-1">
+        Apply
+      </v-btn>
+    </v-app-bar>
     <div class="pl-5" id="namespace_content" v-show="status">
       <v-radio-group class="ma-1" ripple v-model="selectedNs" @change="shouldApplyChange">
-        <v-radio v-for="item in namespaces" :key="item.metadata.uid" :label="item.metadata.name" :value="item.metadata.name" ripple>
+        <v-radio v-for="item in filteredNamespaces" :key="item.metadata.uid" :label="item.metadata.name" :value="item.metadata.name" ripple>
         </v-radio>
       </v-radio-group>
     </div>
     <v-card v-if="!status" outlined>
       <v-card-text>{{ message }}</v-card-text>
     </v-card>
-    <v-footer inset app tile color="white" elevation="4" class="pa-4">
-      <v-btn :disabled="!changed" @click="updateItem" ripple tile color="teal lighten-1">
-        Apply
-      </v-btn>
-    </v-footer>
   </div>
 </template>
         
 <script>
 import ElectronStore from 'electron-store'
 const store = new ElectronStore();
+
+import { debounce } from '../js/helpers.js'
 
 import { createNamespacedHelpers } from 'vuex'
 
@@ -33,7 +37,8 @@ export default {
     return {
       selectedNs: null,
       changed: false,
-      refresh: false
+      refresh: false,
+      filter: ""
     };
   },
   computed: {
@@ -42,6 +47,11 @@ export default {
       status: 'getStatus',
       namespaces: 'orderedNamespaceItems',
     }),
+    filteredNamespaces: function() {
+      return this.namespaces.filter((val) => {
+        return val.metadata.name.includes(this.filter)
+      })
+    }
   },
   activated() {
     this.init();
@@ -66,9 +76,26 @@ export default {
       store.set("selected.namespace", this.selectedNs);
       this.$router.push("/pod")
     },
+    filterNamespace: function(input) {
+      debounce(this.filter = input ? input : "", 200, false);
+    },
     shouldApplyChange: function() {
       this.changed = true;
     }
   },
 };
 </script>
+
+<style scoped>
+.top-sticky {
+  position: sticky;
+  top: 64px;
+  background: white;
+  z-index: 10;
+  max-height: 56px;
+}
+
+.maxwidth {
+  max-width: 350px;
+}
+</style>
